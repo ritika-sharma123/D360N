1aaba47b78ebb5ce95e9c65dbbc7597d62e0fe4f
ritika-sharma123 <Ritika.Sharma@coforge.com>
2024-02-22 18:03:51 +0530

    Added(core):: Id routes to manage integrations

--- src/pages/ManageIntegrations/ManageIntegrationInitiate/index.js
+++ src/pages/ManageIntegrations/ManageIntegrationInitiate/index.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import DataTable from "../../../components/DataTable";
+import { sampleColumnData, sampleRowData } from "../../../mocks/SampleData";
 
 const ManageIntegrationInitiate = () => {
-  return <div className="home">ManageIntegrationInitiate</div>;
+  const { id } = useParams();
+  const [rowDetail, setRowDetail] = useState({});
+
+  useEffect(() => {
+    const rowDetails = sampleRowData.find((i) => i.id === +id);
+    setRowDetail(rowDetails);
+  }, [id]);
+
+  return (
+    <>
+      <div className="home">
+        <h2>{rowDetail.integrations}</h2>
+        <br></br>
+        <DataTable rows={sampleRowData} columns={sampleColumnData} />
+      </div>
+    </>
+  );
 };
 
 export default ManageIntegrationInitiate;
