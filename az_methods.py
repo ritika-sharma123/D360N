d7005768e990a2698ba9e34b90d8771d3c928771
dsvcharanharsha <159875264+dsvcharanharsha@users.noreply.github.com>
2024-02-24 15:06:41 +0530

    az login and small changes
    az login and small changes

---
+++ az_methods.py
@@ -0,0 +1,10 @@
+import subprocess
+
+
+def azlogin():
+    try:
+        subprocess.run(['az','login'], check=True, shell=True,stdout=subprocess.PIPE)
+        return {"status":"Succesfully Logged in"}
+    except Exception as e:
+        print(f"Error in execution: {e}")
+        return {"status":"Credential Mismatch"}
